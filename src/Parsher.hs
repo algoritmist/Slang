668760a62bd6d433aefe317cb3095258115d7f94
Viacheslav Baranov <barash.2018@yandex.ru>
2023-01-10 17:01:38 +0300

    added two char operation support

--- src/Parsher.hs
+++ src/Parsher.hs
@@ -1,26 +1,38 @@
 module Parsher where
 
-import Language
 import Data.Char
+import Language
 
 clex :: String -> [Token]
 
-syntax :: [Token] -> CoreProgram
+--syntax :: [Token] -> CoreProgram
 
-parse :: String -> CoreProgram
+--parse :: String -> CoreProgram
 
-parse = syntax . clex
+--parse = syntax . clex
 
 type Token = String
 
 clex [] = []
+clex [c] = clex [c, ' ']
+--clex [c1, c2] = clex [c1, c2, ' ']
+clex (c1 : c2 : cs)
+  | isSeparator c1 =
+    let separator_cs = dropWhile isSeparator (c2 : cs)
+     in clex separator_cs
+  | isDigit c1 =
+    let num_token = c1 : takeWhile isDigit (c2 : cs)
+        digit_cs = dropWhile isDigit (c2 : cs)
+     in num_token : clex digit_cs
+  | isAlpha c1 =
+    let alpha_cs = dropWhile isAlpha cs
+        var_token = c1 : takeWhile isAlpha cs
+     in var_token : clex alpha_cs
+  | isTwoCharOp [c1, c2] = [c1, c2] : clex cs
+  | otherwise = [c1] : clex (c2 : cs)
 
-clex (c:cs)
-  | isSeparator c = clex cs
-  | isDigit c = num_token : clex rest_cs where
-      num_token = c: takeWhile isDigit cs
-      rest_cs = dropWhile isDigit cs
-  | isAlpha c = var_token : clex rest_cs where
-      var_token = c : takeWhile isAlpha cs
-      rest_cs = dropWhile isAlpha cs
-  | otherwise = [c] : clex cs
+twoCharOps :: [String]
+twoCharOps = ["==", "~=", ">=", "<=", "->"]
+
+isTwoCharOp :: String -> Bool
+isTwoCharOp str = str `elem` twoCharOps
