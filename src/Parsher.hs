f813e22ead88fc4a3f662446a811e0461d004a4c
Viacheslav Baranov <barash.2018@yandex.ru>
2023-01-10 17:44:40 +0300

    added line number support

--- src/Parsher.hs
+++ src/Parsher.hs
@@ -11,25 +11,29 @@ clex :: String -> [Token]
 
 --parse = syntax . clex
 
-type Token = String
+type Token = (Int, String)
 
-clex [] = []
-clex [c] = clex [c, ' ']
+clex cs = helper cs 0
+
+helper :: String -> Int -> [Token]
+helper [] _ = []
+helper [c] line = helper [c, ' '] line
 --clex [c1, c2] = clex [c1, c2, ' ']
-clex (c1 : c2 : cs)
+helper (c1 : c2 : cs) line
+  | c1 == '\n' = helper (c2 : cs) (line + 1)
   | isSeparator c1 =
     let separator_cs = dropWhile isSeparator (c2 : cs)
-     in clex separator_cs
+     in helper separator_cs line
   | isDigit c1 =
-    let num_token = c1 : takeWhile isDigit (c2 : cs)
+    let num_token = (line, c1 : takeWhile isDigit (c2 : cs))
         digit_cs = dropWhile isDigit (c2 : cs)
-     in num_token : clex digit_cs
+     in num_token : helper digit_cs line
   | isAlpha c1 =
     let alpha_cs = dropWhile isAlpha cs
-        var_token = c1 : takeWhile isAlpha cs
-     in var_token : clex alpha_cs
-  | isTwoCharOp [c1, c2] = [c1, c2] : clex cs
-  | otherwise = [c1] : clex (c2 : cs)
+        var_token = (line, c1 : takeWhile isAlpha cs)
+     in var_token : helper alpha_cs line
+  | isTwoCharOp [c1, c2] = (line, [c1, c2]) : helper cs line
+  | otherwise = (line, [c1]) : helper (c2 : cs) line
 
 twoCharOps :: [String]
 twoCharOps = ["==", "~=", ">=", "<=", "->"]
