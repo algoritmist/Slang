34a40fb8854d8edc6310b7eb7ed65d4834cffaec
Viacheslav Baranov <barash.2018@yandex.ru>
2023-01-11 16:52:34 +0300

    modified cabal config and implemeted better comment filter

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib
     ( someFunc
     ) where
+import Parser
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn $ show $ clex "3 == 5 - 2 \n--this is a comment\n y = x^2\n"
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,7 +1,8 @@
-module Parsher where
+module Parser where
 
 import Data.Char
 import Language
+import Text.Regex.TDFA
 
 clex :: String -> [Token]
 
@@ -13,27 +14,30 @@ clex :: String -> [Token]
 
 type Token = (Int, String)
 
-clex cs = helper cs 0
+clex s = helper 0 $ filterComments s
 
-helper :: String -> Int -> [Token]
-helper [] _ = []
-helper [c] line = helper [c, ' '] line
+helper :: Int -> String -> [Token]
+helper _ [] = []
+helper line [c] = helper line [c, ' ']
 --clex [c1, c2] = clex [c1, c2, ' ']
-helper (c1 : c2 : cs) line
-  | c1 == '\n' = helper (c2 : cs) (line + 1)
+helper line (c1 : c2 : cs)
+  | c1 == '\n' = helper (line + 1) (c2 : cs)
   | isSeparator c1 =
     let separator_cs = dropWhile isSeparator (c2 : cs)
-     in helper separator_cs line
+     in helper line separator_cs
   | isDigit c1 =
     let num_token = (line, c1 : takeWhile isDigit (c2 : cs))
         digit_cs = dropWhile isDigit (c2 : cs)
-     in num_token : helper digit_cs line
+     in num_token : helper line digit_cs
   | isAlpha c1 =
     let alpha_cs = dropWhile isAlpha cs
         var_token = (line, c1 : takeWhile isAlpha cs)
-     in var_token : helper alpha_cs line
-  | isTwoCharOp [c1, c2] = (line, [c1, c2]) : helper cs line
-  | otherwise = (line, [c1]) : helper (c2 : cs) line
+     in var_token : helper line alpha_cs
+  | isTwoCharOp [c1, c2] = (line, [c1, c2]) : helper line cs
+  | otherwise = (line, [c1]) : helper line (c2 : cs)
+
+filterComments :: String -> String
+filterComments = unlines . filter (\x -> not $ x =~ "(--.*)" :: Bool) . lines
 
 twoCharOps :: [String]
 twoCharOps = ["==", "~=", ">=", "<=", "->"]
